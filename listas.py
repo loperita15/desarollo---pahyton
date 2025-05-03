a1a0d0fa29d4269448cea8b86a07b6706dc7be77
kevin <esmoto130@gmail.com>
2025-05-03 13:43:36 -0500

    aprendiendo sobre listas

--- listas.py
+++ listas.py
@@ -1,1 +1,19 @@
 #este es un espacio para ablar de listas 
+#crear una lista con los meses del año 
+meses =["enero","febrero","marzo","abril","mayo","junio","julio","sectiembre","octubre","noviembre","diciembre" ]
+print(meses[0])
+print(meses[-1]) 
+print(meses[3:6])
+#agregar un dia de la semana a la lista
+meses.append("martes")
+print(meses)
+del(meses[11])
+print(meses)
+len(meses)
+print(len(meses))
+meses.clear()
+print(meses)
+del meses
+meses=[]
+meses.append("enero")
+print(meses)
